e9eb87f1e3abbbf8b1ffe7dbbf18c160f5874d6e
j-m-smith426 <joabsmith4226@gmail.com>
2022-09-26 12:12:17 -0400

    updated configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import { Navbar } from './components/Navbar'
-import classes from './App.module.css'
-// import { About } from './pages/About'
-// import { Home } from './pages/Home'
+import { Navbar } from "./components/Navbar";
+import classes from "./App.module.css";
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <div className={classes.container}>
-        <Navbar />
-        {/* <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-        </Switch> */}
-      </div>
-    </BrowserRouter>
-  )
-}
+	return (
+		<BrowserRouter>
+			<div className={classes.container}>
+				<Navbar />
+			</div>
+		</BrowserRouter>
+	);
+};
 
-export default App
+export default App;
